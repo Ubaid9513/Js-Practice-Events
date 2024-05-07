b2903b013641415f9509e0c0df25b64ddb00ddd7
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-05-07 05:16:03 +0500

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function greet(){
-    alert("Assalam O Aliakum");
-}
+// function greet(){
+//     alert("Assalam O Aliakum");
+// }
 
 
 // function changebg(element) {
@@ -30,33 +30,50 @@ function greet(){
 // var form = document.getElementById("productForm");
 // form.addEventListener("submit", checkAddress);
 
+  // ---X---
 
-var fullPara =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option hidden ratione debitis temporibus id recusandae labore maiores iure, ullam dicta velit ipsam possimus laborum fugit. In, nemo ad! Nobis, id. Deserunt ab accusantium iure suscipit culpa fugit quaerat asperiores. <a href='Javascript: void(0)' onclick='shrinkPara()'>see less</a>";
-var shortpara =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option  <a href='Javascript: void(0)' onclick='expandPara()'>see more</a>";
+// var fullPara =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option hidden ratione debitis temporibus id recusandae labore maiores iure, ullam dicta velit ipsam possimus laborum fugit. In, nemo ad! Nobis, id. Deserunt ab accusantium iure suscipit culpa fugit quaerat asperiores. <a href='Javascript: void(0)' onclick='shrinkPara()'>see less</a>";
+// var shortpara =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option  <a href='Javascript: void(0)' onclick='expandPara()'>see more</a>";
 
-var para = document.getElementById("para");
-function expandPara() {
-    // console.log(para.innerText);
-    // console.log(para.innerHTML);
+// var para = document.getElementById("para");
+// function expandPara() {
+//     // console.log(para.innerText);
+//     // console.log(para.innerHTML);
 
-  para.innerHTML = fullPara;
-}
-function shrinkPara() {
-  para.innerHTML = shortpara;
-}
+//   para.innerHTML = fullPara;
+// }
+// function shrinkPara() {
+//   para.innerHTML = shortpara;
+// }
 
-var fullPara2 =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option hidden ratione debitis temporibus id recusandae labore maiores iure, ullam dicta velit ipsam possimus laborum fugit. In, nemo ad! Nobis, id. Deserunt ab accusantium iure suscipit culpa fugit quaerat asperiores. <a href='Javascript: void(0)' onclick='shrinkPara2()'>see less</a>";
-var shortPara2 =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option  <a href='Javascript: void(0)' onclick='expandPara2()'>see more</a>";
+// var fullPara2 =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option hidden ratione debitis temporibus id recusandae labore maiores iure, ullam dicta velit ipsam possimus laborum fugit. In, nemo ad! Nobis, id. Deserunt ab accusantium iure suscipit culpa fugit quaerat asperiores. <a href='Javascript: void(0)' onclick='shrinkPara2()'>see less</a>";
+// var shortPara2 =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, recusandae maxime. Ullam repellat voluptatem porro quibusdam option  <a href='Javascript: void(0)' onclick='expandPara2()'>see more</a>";
 
-var para2 = document.getElementById("para2");
-function expandPara2(){
-    para2.innerHTML = fullPara2;
-}
-function shrinkPara2(){
-    para2.innerHTML = shortPara2;
-}
+// var para2 = document.getElementById("para2");
+// function expandPara2(){
+//     para2.innerHTML = fullPara2;
+// }
+// function shrinkPara2(){
+//     para2.innerHTML = shortPara2;
+// }
 
+  // ---X---
+
+//   function getName(){
+//   var name = document.getElementById("xyz")
+//   console.log(name.value);
+//   name.value = "";
+// }
+
+
+// function setName(){
+//   var name = document.getElementById("xyz");
+//   name.value = "Ubaid";
+//   var para = document.getElementById("para");
+//   para.innerHTML = "Hello! This is example"
+// }
+// setName();
